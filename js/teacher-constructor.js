a06e28858db68e4c88f48cbf72bd60eb678fc570
Joseph Barten <josbarten@wl.k12.ia.us>
2018-11-16 13:45:22 -0600

    teachers display

--- js/teacher-constructor.js
+++ js/teacher-constructor.js
@@ -1,22 +1,23 @@
 'use strict';
 
-var REPLACE1 = [];
+var teacher_list = [];
+var Teacher = function(teacher){
+    this.image = teacher.image;
+    this.name = teacher.name;
+    this.teacher_id = teacher.teacher_id;
+    this.subjects = teacher.subjects;
+}
 
-var REPLACE2 = function(teacher){
-    
-};
-
-REPLACE2.prototype.toHtml = function(){
+Teacher.prototype.toHtml = function(){
     var source = $('#teacher-template').text();
     var template = Handlebars.compile(source);
     return template(this);
 };
 
-teachers.forEach(function(REPLACE3){
-    REPLACE1.push(new REPLACE2(REPLACE3));
+teachers.forEach(function(teacher){
+    teacher_list.push(new Teacher(teacher));
 });
 
-
-REPLACE1.forEach(function(REPLACE3){
-    $('#teacher-listings').append(REPLACE3.toHtml());
+teacher_list.forEach(function(teacher){
+    $('#teacher-listings').append(teacher.toHtml());
 });
---
+++ js/teachers.js
@@ -0,0 +1,33 @@
+var teachers = [
+{
+    image: "hauschildt.jpg",
+    name: "Mark haushildt",
+    subjects: ["marketing","keyboarding"],
+    teacher_id: "1000",
+},
+{
+    image: "hood.jpg",
+    name: "Betsey Hood",
+    subjects: ["cooking","child developement"],
+    teacher_id: "1001",
+},
+{
+    image: "symmonds.jpg",
+    name: "Olivia Symmonds",
+    subjects: ["Biology","AP Bio"],
+    teacher_id: "1002",
+},
+{
+    image: "gunn.jpg",
+    name: "Mike Gunn",
+    subjects: ["science","physics"],
+    teacher_id: "1003",
+},
+{
+    image: "diemer.jpg",
+    name: "Curt Diemer",
+    subjects: ["History"],
+    teacher_id: "1004",
+},
+];
+
